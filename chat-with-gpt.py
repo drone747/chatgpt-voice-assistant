f34580a1bb9b081de26a1efa4e972c655bb93190
drone747 <106736577+drone747@users.noreply.github.com>
2024-03-28 20:07:13 +0530

    Update chat-with-gpt.py

--- chat-with-gpt.py
+++ chat-with-gpt.py
@@ -17,7 +17,7 @@ def SpeakText(command):
     engine.runAndWait()
 
 # Your OpenAI API key
-client = OpenAI(api_key="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")
+client = OpenAI(api_key="YOUR-API-KEY")
 
 SpeakText("Hi, My name is bobert")
 time.sleep(1)
